ba619c52d83a1b9aec6f7426c0666b499eb14669
JohnJaccard <Jackofswiss@gmail.com>
2023-11-12 13:58:44 +0100

    v1..5
    button problem ✔️
    mora from materials added to the first mora

--- characterpage.js
+++ characterpage.js
@@ -3,29 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Parse the character data from the URL parameter
     var urlParams = new URLSearchParams(window.location.search);
-    var characterData = urlParams.get('character');
-
-    if (characterData) {
-        var character = JSON.parse(decodeURIComponent(characterData));
-
-        // Display the character information
-        var characterName = document.createElement('h2');
-        characterName.textContent = character.DisplayName;
-
-        var characterElement = document.createElement('p');
-        characterElement.textContent = 'Element: ' + character.Element;
-
-        var characterRarity = document.createElement('p');
-        characterRarity.textContent = 'Rarity: ' + character.Rarity + ' Stars';
-
-        // Add more character information as needed
-
-        // Append the elements to the characterInfo div
-        characterInfo.appendChild(characterName);
-        characterInfo.appendChild(characterElement);
-        characterInfo.appendChild(characterRarity);
-        // Append more character information as needed
-    } else {
-        characterInfo.textContent = 'Character not found.';
-    }
 });
